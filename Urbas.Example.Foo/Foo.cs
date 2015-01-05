01297f4a19c0dbf872e72554abdcc3274444d866
Matej Urbas <matej.urbas@gmail.com>
2015-01-05 15:33:35 +0100

    Incremented the version.

--- Urbas.Example.Foo/Foo.cs
+++ Urbas.Example.Foo/Foo.cs
@@ -3,6 +3,7 @@
 namespace Urbas.Example.Foo {
   public class Foo {
     public string Message { get { return "I am Foo!"; } }
+    public string WhatIf { get { return "This property mocks NuGet IPackage.WhatIf. Foo!"; } }
   }
 }
 
